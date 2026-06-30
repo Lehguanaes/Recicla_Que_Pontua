3a5ee929bc0b6288108f482639a2a97e36c0fb70
lehguanaes <lehguanaes@gmail.com>
2026-06-30 20:21:39 -0300

    fix: responsividade footer

--- src/components/rodape/Rodape.jsx
+++ src/components/rodape/Rodape.jsx
@@ -4,10 +4,12 @@ const Rodape = () => {
   return (
     <footer className="rodape">
       <p>
-        © 2026 <strong>Recicla Que Pontua</strong>. Todos os direitos reservados.
+        © 2026 <strong>Recicla Que Pontua</strong>.
+        <br className="quebra-mobile" />
+        Todos os direitos reservados.
       </p>
     </footer>
   );
-}
+};
 
 export default Rodape;
